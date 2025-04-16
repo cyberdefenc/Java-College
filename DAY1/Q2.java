984dd992e4e52b08b79bb48b7ed76bfa72c98e87
UtkarshcyberArena <utkarshkushwahaofficial29@gmail.com>
2025-04-16 22:42:56 +0530

    Added .gitignore and updated Q2.java

--- DAY1/Q2.java
+++ DAY1/Q2.java
@@ -1,13 +1,16 @@
 package DAY1;
 
 public class Q2 {
-     
 
-        public static void main(String[] args) 
-        {
-          
-            System.out.println("Hello, World!"); 
-        }
-    
+    public static void main(String args[]){
+        int n=5;
+        if(n%2==0)
+        System.out.println(n+" is even");
+
+        else
+        System.out.println(n+" is odd");
+
+
+    }
     
 }
