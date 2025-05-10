3c00b077bdb58bcc1cab6970f610629eaeda1ed0
UtkarshcyberArena <utkarshkushwahaofficial29@gmail.com>
2025-05-10 10:30:42 +0530

    Important programs added

---
+++ DAY1/P2_ev_od_.java
@@ -0,0 +1,18 @@
+// Java program to check whether a number is even or odd
+
+package DAY1;
+
+public class P2_ev_od_ {
+
+    public static void main(String args[]){
+        int n=5;
+        if(n%2==0)
+        System.out.println(n+" is even");
+
+        else
+        System.out.println(n+" is odd");
+
+
+    }
+    
+}
---
+++ Day2/P1_arr_print_.java
@@ -0,0 +1,14 @@
+//Print and display array with  values given
+package Day2;
+import java.util.Arrays; //in java you can not print array values directly by just calling its name
+
+
+public class P1_arr_print_ {
+
+    public static void main(String[] args) {
+    
+    int[] arr={1,2,3,4,5,6,7,8,9,10}; //also can be written as int arr[]=
+    System.out.println("Your values are = " + Arrays.toString(arr));
+        
+    }
+}
