b541f473a49e882da563f122f9cff46d5efb2a77
Utkarsh10$# <utkarshkushwahaofficial29@gmail.com>
2025-05-21 10:48:14 +0530

    Interface

---
+++ Day10/P1_ambuigity_.java
@@ -0,0 +1,40 @@
+// P1. Multiple Inheritance problem in Java(Ambuigity)
+
+       // TODO Auto-generated method stub
+        // Ambiguity in Multiple Inheritance
+        // class A
+        // class B extends A
+        // class C extends A
+
+
+class A {
+    void sum() {
+        System.out.println("I am A");
+    }
+}
+
+class B{
+    void sum() {
+        System.out.println("I am B");
+    }
+
+}
+
+class C extends A,B // Will throw Abugity error
+{
+    void sum() {
+        System.out.println("I am C");
+    }
+}
+
+class P1_ambuigity_{
+
+
+    public static void main(String[] args) {
+ 
+        C obj=new C();
+        obj.sum(); // Ambiguity error
+
+}
+
+}
---
+++ Day10/P2_interface_.java
@@ -0,0 +1,34 @@
+// P2. Program to demonstrate Interface in Java
+
+
+interface Clinet 
+{
+    void print();
+    void show();
+
+}
+
+
+class Dev implements Clinet 
+{
+    public void print() 
+    {
+        System.out.println("Print the bill");
+    }
+    public void show() 
+    {
+        System.out.println("Show the menu");
+    }
+}
+
+public class P2_interface_ 
+{
+
+    public static void main(String[] args)
+     {
+        Dev d = new Dev();
+        d.print();
+        d.show();
+     }
+    
+}
