bb614eb19377199de5e9db88c81d25bc53204042
Utkarsh10$# <utkarshkushwahaofficial29@gmail.com>
2025-05-24 16:52:58 +0530

    new thing

---
+++ Day12/P3_max_subarr_.java
@@ -0,0 +1,15 @@
+package Day12;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class P3_max_subarr_ {
+    public static void main(String[] args) {
+        
+        int[] = {-4,6,0,-7,3,4,-8};
+        ArrayList<int> arr = new ArrayList<>();
+        Collections.sort(arr);
+        System.out.println("ArrayList: " + arr);
+    
+}
+}
